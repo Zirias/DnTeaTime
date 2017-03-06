31938ee339827aed6ad54438572a46146e15edd6
Felix Palmen <felix@palmen-it.de>
2017-03-06 14:02:41 +0100

    ensure single instance

--- FormsFrontend/Program.cs
+++ FormsFrontend/Program.cs
@@ -2,32 +2,38 @@
 using PalmenIt.dntt.TeaTimer.RegistryRepository;
 using PalmenIt.dntt.TeaTimer.STTimer;
 using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace PalmenIt.dntt.FormsFrontend
 {
     static class Program
     {
+        private static Mutex mutex = new Mutex(true, "{F87FEF1D-AC6C-4488-BDAC-FCC102324539}");
+
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
-            var repository = new TeaTimerRepository();
-            if (repository.Count == 0)
+            if (mutex.WaitOne(TimeSpan.Zero, true))
             {
-                repository.Add(TeaTimerDefinition.Create("Black Tea", 4, 0));
-                repository.Add(TeaTimerDefinition.Create("Earl Grey", 4, 30));
-                repository.Add(TeaTimerDefinition.Create("Herbal Tea", 10, 0));
-                repository.Add(TeaTimerDefinition.Create("Green Tea", 2, 30));
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+
+                var repository = new TeaTimerRepository();
+                if (repository.Count == 0)
+                {
+                    repository.Add(TeaTimerDefinition.Create("Black Tea", 4, 0));
+                    repository.Add(TeaTimerDefinition.Create("Earl Grey", 4, 30));
+                    repository.Add(TeaTimerDefinition.Create("Herbal Tea", 10, 0));
+                    repository.Add(TeaTimerDefinition.Create("Green Tea", 2, 30));
+                }
+
+                var processor = new TeaTimerProcessor();
+
+                var app = new App(repository, processor);
+
+                Application.Run(new TrayAppContext(app));
             }
-
-            var processor = new TeaTimerProcessor();
-
-            var app = new App(repository, processor);
-
-            Application.Run(new TrayAppContext(app));
         }
     }
 }
